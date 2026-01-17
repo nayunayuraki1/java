37d72f4c2e645494d409cd0e67cb9ad88d5e9071
Lucif <n.na.c.c34@gmail.com>
2026-01-18 01:22:35 +0900

    DB_TEST4

--- src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -1,3 +1,5 @@
+import com.example.demo.entity.repository.UserRepository;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
