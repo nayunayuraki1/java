4c0ac490a70b891fbb20ad26055bb81168ee632c
Lucif <n.na.c.c34@gmail.com>
2026-01-18 01:08:01 +0900

    DB_TEST2

--- src/test/java/com/example/demo/UserRepositoryTest.java
+++ src/test/java/com/example/demo/UserRepositoryTest.java
@@ -1,3 +1,14 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
 @SpringBootTest
 @Testcontainers
 public class UserRepositoryTest {
