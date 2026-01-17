a766a443c59a2d6e2bcfd72fb3531ac936eca5ae
Lucif <n.na.c.c34@gmail.com>
2026-01-18 00:57:17 +0900

    DB_TEST

---
+++ src/test/java/com/example/demo/UserRepositoryTest.java
@@ -0,0 +1,32 @@
+@SpringBootTest
+@Testcontainers
+public class UserRepositoryTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+
+        registry.add("spring.flyway.url", postgres::getJdbcUrl);
+        registry.add("spring.flyway.user", postgres::getUsername);
+        registry.add("spring.flyway.password", postgres::getPassword);
+    }
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void testInsertAndFind() {
+        User user = new User(null, "Yuki");
+        userRepository.save(user);
+
+        assertEquals(1, userRepository.count());
+    }
+}
