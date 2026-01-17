3c69d6c0ec16217f7b5728fc3f70464f689e7751
Lucif <n.na.c.c34@gmail.com>
2026-01-18 01:24:49 +0900

    DB_TEST5

--- src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -1,4 +1,4 @@
-import com.example.demo.entity.repository.UserRepository;
+import com.example.demo.repository.UserRepository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
